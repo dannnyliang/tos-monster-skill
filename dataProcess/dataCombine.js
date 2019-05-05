55c88dd372c6c0f742f0f9662dbbca9b27d97a9f
DannyLiang <youchenliang@gmail.com>
2019-05-05 15:51:59 +0800

    feat(dataProcess): write monster.log.json to record transform data infos

--- dataProcess/dataCombine.js
+++ dataProcess/dataCombine.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const prop = require('lodash/fp/prop');
+const prop = require("lodash/fp/prop");
 
 /***** I/O *****/
-const imagesFilePath = './dataProcess/tos-monster-images.json';
-const monstersFilePath = './dataProcess/tos-monsters.json';
-const combinedFilePath = './src/tosMonsters.js';
+const imagesFilePath = "./dataProcess/tos-monster-images.json";
+const monstersFilePath = "./dataProcess/tos-monsters.json";
+const logPath = "./dataProcess/monster-log.json";
+const combinedFilePath = "./src/tosMonsters.js";
 
 const readFilePromise = filename => {
   return new Promise((resolve, reject) => {
@@ -27,7 +28,7 @@ const read = () => {
   console.log(`<----- Read images and monsters ----->`);
   Promise.all([
     readFilePromise(imagesFilePath),
-    readFilePromise(monstersFilePath),
+    readFilePromise(monstersFilePath)
   ]).then(res => {
     console.log(`<----- Read Successful, remove raw files ----->`);
     removeFile(imagesFilePath);
@@ -37,14 +38,43 @@ const read = () => {
 
     const combined = monsters.map(monster => {
       const match = thumbnails.find(
-        ({ monsterId }) => monsterId === monster.monsterId,
+        ({ monsterId }) => monsterId === monster.monsterId
       );
       return {
         ...monster,
-        thumbnail: prop(['thumbnail-src'], match),
+        thumbnail: prop(["thumbnail-src"], match)
       };
     });
+    console.log(`<----- Combined Monster counts: ${combined.length} ----->`);
+    writeLog(monsters, thumbnails, combined);
     write(combined);
+  });
+};
+
+const writeLog = (monsters, thumbnails, combined) => {
+  const now = new Date();
+  const log = {
+    raw: {
+      monsters: {
+        description: "從神魔 wiki 上爬下來經過清理後的召喚獸資訓",
+        total: monsters.length
+      },
+      images: {
+        description: "召喚獸縮圖，用於和 monsters combine",
+        total: thumbnails.length
+      }
+    },
+    transformed: {
+      monsters: {
+        description: "已經 combine 好的資料",
+        total: combined.length
+      }
+    },
+    upadteTime: `${now.getFullYear()}-${now.getMonth()}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+  };
+
+  fs.writeFile(logPath, JSON.stringify(log), err => {
+    if (err) throw err;
   });
 };
 
@@ -58,7 +88,7 @@ const write = raw => {
   `,
     err => {
       if (err) throw err;
-    },
+    }
   );
 };
 
